8facf2d6fa7838416af64738aff2e8ff61044d4d
Tarik <t01012100@gmail.com>
2018-09-18 23:35:34 +0200

    disable settings for static folder

--- football_api/settings.py
+++ football_api/settings.py
@@ -119,9 +119,9 @@ STATIC_URL = '/static/'
 django_heroku.settings(locals())
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
 
 # enable gzip
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
